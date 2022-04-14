8e75e58f3c121b0e73bdc7a05c888a66ff3b51b8
Bianca Celestino <biancafrcm@gmail.com>
2022-04-14 14:03:02 -0300

    segundocommit

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ function required()
 {
     var nome = document.getElementById('nome').value;
 
+    var cb = document.getElementById('checkbox');
 
-    if (nome === ""){
+
+    if (nome === '' && cb.checked == false){
         alert("Por favor, preencha os dados");
         return false;
     }
@@ -14,3 +16,40 @@ function required()
         return true; 
     }
 }
+
+
+/*
+
+
+
+
+function validacao(){
+    var nome = document.getElementById('nome').value
+
+    var email = document.getElementById('email').value
+    
+    var cpf = document.getElementById('cpf').value
+
+    if(nome == null || nome == '') {
+        alert("Preencha o nome")
+        return false
+    }
+
+}
+
+function validacao(){
+    var nome = document.forms["form"].value
+    if (nome == ''){
+        alert('Preencha os espaços')
+        return false
+    }
+    else {
+        alert('boa, espaços preenchidos')
+        return true
+    }
+}
+
+
+
+
+*/
